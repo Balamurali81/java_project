e198ccf7bc137522df31215c047b2e53dcfd7c90
Balamurali81 <n.balamuralikrishna@gmail.com>
2021-05-05 20:08:01 +0530

    branch change

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -3,7 +3,7 @@ job('First-seed-project-Maven') {
     // logRotator(-1, 10)
     // jdk('Java 8')
     scm {
-        git('https://github.com/Balamurali81/java_project.git', 'main')
+        git('https://github.com/Balamurali81/java_project.git', 'simple')
     }
     triggers {
         scm('* * * * *')
