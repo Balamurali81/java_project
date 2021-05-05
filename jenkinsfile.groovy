2048018567a1d87c966b7766b24f16db2849eb64
Balamurali81 <n.balamuralikrishna@gmail.com>
2021-05-05 19:26:37 +0530

    DSL file added

---
+++ jenkinsfile.groovy
@@ -0,0 +1,16 @@
+freeStyleJob('First-seed-project-Maven') {
+    // logRotator(-1, 10)
+    // jdk('Java 8')
+    scm {
+        git('https://github.com/Balamurali81/java_project.git', 'main')
+    }
+    triggers {
+        git('* * * * *')
+    }
+    steps {
+        Maven('clean build', 'java-tomcat-sample/pom.xml')
+    }
+    publishers {
+        archiveArtifacts('**/*.war')
+    }
+}
